0ae1e22273a056ff9af6fa4c44899ad5ae209846
Holden Mills <mholdenmills@gmail.com>
2015-11-30 23:30:18 -0500

    Refactors javascript (formatting and cleans up comments)
    Deletes oldstuff, game logic, jquery, solution and renamed READ.ME

--- javascript.js
+++ javascript.js
@@ -1,55 +1,32 @@
- //ruby -r un -e httpd . -p 5000//
-
-
-//Step 1: determine when page loads
-//runs function after (document) loads treats page like a giant function
 $(document).ready(function() {
 
-  //Step 2: create an array of squares
-  // creates array of empty strings
   var box = ['','','','','','','','',''];
-
-//$('main-button').html('Click to Play')
-  //sets X to last clicked (selected) box
   var lastSelected = 'X';
-//Step 3: Attach a click hander to each square
-//Attaches click handler to parent 'grid'
-//# denotes id element which is a child of div class board
-//.on is the click handler,  "click" is action?
-//$ represents jquery getting elemment from index.html
-  $( "#grid").on( "click", function(event) {
-    // determine the element that saw the click event, it's a button
-    //event is the click handler, target is the button
-    var elementSelected = event.target;
-    //Step 4: Get the cell number clicked
-    //getting cell number selected by the click from line 15
-    var cellNumberClicked = $(elementSelected).data('cell');
 
+  $( "#grid").on( "click", function(event) {
+    var elementSelected = event.target;
+    var cellNumberClicked = $(elementSelected).data('cell');
     console.log("click" + cellNumberClicked);
-    //Step 5: Change the cell HTML to X or O
-    //This code alternates between X and O
+
     if(lastSelected === 'X') {
-      $(elementSelected).html('X');
       getWinner(lastSelected);
+      $(elementSelected).html('X');
         lastSelected = 'O';
-        //Step 5: add the X or O to boxes
         box[cellNumberClicked] = 'X';
     }
-    //If previous element was O then return X
+
     else {
       $(elementSelected).html('O');
       getWinner(lastSelected);
         lastSelected = 'X';
         box[cellNumberClicked] = 'O';
     }
-  }
-  //Step 6: check for winner
-  // Checks for all possible winning combinations with X or O
-  );
+  });
 
- var boxes = $('#grid').children();
+  var boxes = $('#grid').children();
 
   var getWinner = function getWinner(lastSelected) {
+
      //rows
     for(var index = 0; index < 3; index++) {
       if (boxes.eq(3 * index).text() === lastSelected &&
@@ -87,9 +64,9 @@ $(document).ready(function() {
         alert('Player ' + lastSelected + ' won!');
         return true;
     }
-
   };
- // for(var index = 0; index < 9; index++) {
+
+  // for(var index = 0; index < 9; index++) {
   if (getWinner === false &&
     box[0] !== '' &&
     box[1] !== '' &&
