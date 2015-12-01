b3577c4425e8880e84e2cb98a30af115cbfc6460
Holden Mills <mholdenmills@gmail.com>
2015-11-30 23:16:40 -0500

    Works on tie function - not working

--- javascript.js
+++ javascript.js
@@ -53,42 +53,58 @@ $(document).ready(function() {
      //rows
     for(var index = 0; index < 3; index++) {
       if (boxes.eq(3 * index).text() === lastSelected &&
-         boxes.eq(3 * index + 1).text() === lastSelected &&
-         boxes.eq(3 * index + 2).text() === lastSelected &&
-         boxes.eq(3 * index).text() !== '')
-         {
-          console.log(lastSelected + 'won rows');
+        boxes.eq(3 * index + 1).text() === lastSelected &&
+        boxes.eq(3 * index + 2).text() === lastSelected &&
+        boxes.eq(3 * index).text() !== '')
+        {
+          alert('Player ' + lastSelected + ' won!');
           return true;
-
       }
     }
 
-  // cols
- for (index = 0; index < 3; index++) {
-   if (boxes.eq(index).text() === lastSelected &&
-       boxes.eq(index + 3).text() === lastSelected &&
-       boxes.eq(index + 6).text() === lastSelected &&
-       boxes.eq(index).text() !== '')
-       {
-        console.log(lastSelected + 'won cols');
+    // cols
+    for (index = 0; index < 3; index++) {
+      if (boxes.eq(index).text() === lastSelected &&
+        boxes.eq(index + 3).text() === lastSelected &&
+        boxes.eq(index + 6).text() === lastSelected &&
+        boxes.eq(index).text() !== '')
+        {
+          alert('Player ' + lastSelected + ' won!');
+          return true;
+      }
+    }
+
+   // diag
+   if ((boxes.eq(0).text() === lastSelected &&
+      boxes.eq(4).text() === lastSelected &&
+      boxes.eq(8).text() === lastSelected &&
+      boxes.eq(8).text() !== '') ||
+      (boxes.eq(2).text() === lastSelected &&
+      boxes.eq(4).text() === lastSelected &&
+      boxes.eq(6).text() === lastSelected &&
+      boxes.eq(2).text() !== ''))
+      {
+        alert('Player ' + lastSelected + ' won!');
         return true;
     }
-  }
 
- // diag
- if ((boxes.eq(0).text() === lastSelected &&
-    boxes.eq(4).text() === lastSelected &&
-    boxes.eq(8).text() === lastSelected &&
-    boxes.eq(8).text() !== '') ||
-    (boxes.eq(2).text() === lastSelected &&
-    boxes.eq(4).text() === lastSelected &&
-    boxes.eq(6).text() === lastSelected &&
-    boxes.eq(2).text() !== ''))
+  };
+ // for(var index = 0; index < 9; index++) {
+  if (getWinner === false &&
+    box[0] !== '' &&
+    box[1] !== '' &&
+    box[2] !== '' &&
+    box[3] !== '' &&
+    box[4] !== '' &&
+    box[5] !== '' &&
+    box[6] !== '' &&
+    box[7] !== '' &&
+    box[8] !== '' )
     {
-      console.log(lastSelected + 'won diags');
+      alert('Tie');
       return true;
-  }
+    };
   return false;
-};
-
 });
+
+
