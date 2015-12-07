9be95afddae02e7a2e28be2e8c34fa0581b0a4f4
Holden Mills <mholdenmills@gmail.com>
2015-12-07 15:19:57 -0500

    Toggle function working, mark cell not working
    Added getWinner functions and the board stopped showing tokens

--- javascript.js
+++ javascript.js
@@ -1,79 +1,98 @@
 $(document).ready(function() {
-  $(".board").on("click",":button",function(event) {
 
-var game = {
-  board: ['','','','','','','','',''],
-  token: 'X'
-};
+  var game = {
+    board: ['','','','','','','','',''],
+    token: 'X'
+  };
 
-var markCell = function markCell (element) {
-  updateBoard($(element).data('cell'));
-  updateUi(element);
-};
-var updateBoard = function updateBoard (index) {
-  game.board[index] = game.token;
-};
+  $(".board").on("click",":button",function() {
 
-var updateUi = function updateUi (element) {
-  $(element).html(game.token);
-};
+    var playGame = function (element) {
+      var markCell = function markCell (element) {
+        updateBoard($(element).data('cell'));
+        updateUi(element);
+      };
 
-var toggleTurn = function toggleTurn () {
-  game.token === 'O' ? game.token = 'X' : game.token = 'O';
-};
+      var updateBoard = function updateBoard (index) {
+        game.board[index] = game.token;
+        checkWinner(game);
+      };
 
+      var updateUi = function updateUi (element) {
+        markCell(event.target);
+        $(element).html(game.token);
+      };
 
+      var toggleTurn = function toggleTurn (element) {
+        game.token === 'X' ? game.token = 'O' : game.token = 'X';
+      };
+    };
 
-var checkRows = function checkRows () {
-  game.board.slice(0, 3).every(function (cell) {
-    return cell === game.token;
+    var checkRows = function checkRows () {
+      game.board.slice(0, 3).every(function (cell) {
+        return cell === game.token;
+      });
+      game.board.slice(3, 6).every(function (cell) {
+        return cell === game.token;
+      });
+      game.board.slice(6, 9).every(function (cell) {
+        return cell === game.token;
+      });
+      return false;
+    };
+
+    var checkColumns = function checkColumns () {
+      if (([game.board[2], game.board[5], game.board[8]].every(updateBoard)) === game.token ||
+         ([game.board[2], game.board[5], game.board[8]].every(updateBoard)) === game.token ||
+         ([game.board[2], game.board[5], game.board[8]].every(updateBoard)) === game.token) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    };
+
+    var checkDiagonals = function checkDiagonals () {
+      if (([game.board[0], game.board[4], game.board[8]].every(updateBoard)) === game.token ||
+         ([game.board[2], game.board[4], game.board[6]].every(updateBoard)) === game.token) {
+      return true;
+      }
+      else {
+        return false;
+      }
+    };
+
+playGame(event.target);
+
   });
-
-  game.board.slice(3, 6).every(function (cell) {
-    return cell === game.token;
-  });
-
-  game.board.slice(6, 9).every(function (cell) {
-    return cell === game.token;
-  });
-
-  return false;
-};
-
-var checkColumns = function checkColumns () {
-  if (([game.board[2], game.board[5], game.board[8]].every(updateBoard)) === game.token ||
-     ([game.board[2], game.board[5], game.board[8]].every(updateBoard)) === game.token ||
-     ([game.board[2], game.board[5], game.board[8]].every(updateBoard)) === game.token) {
-    return true;
-    }
-  else {
-    return false;
-  }
-};
-
-var checkDiagonals = function checkDiagonals () {
-  if (([game.board[0], game.board[4], game.board[8]].every(updateBoard)) === game.token ||
-     ([game.board[2], game.board[4], game.board[6]].every(updateBoard)) === game.token) {
-  return true;
-  }
-  else {
-    return false;
-  }
-};
-
-var checkWinner = function checkWinner () {
-  // check functions should return a token or null
-  if (checkRows() || checkColumns() || checkDiagonals()) {
-    alert(game.token + ' is the winner!');
-  }
-};
-
-
-
-  markCell(event.target)
-  toggleTurn();
-
-  checkWinner();
 });
-});
+
+//   if (game.token === 'X') {
+//     game.token = 'O';
+//     console.log(game.token);
+//   }
+//   else {
+//     game.token = 'X';
+//   };
+//   console.log(game.token);
+// };
+
+
+
+
+
+
+
+
+
+// var checkTie = function checkTie() {
+//   if checkWinner === false && game.board.every(!="") {
+//     alert("It's a tie!  Play again!");
+//   }
+// };
+
+// checkWinner(event.target);
+
+
+
 
