d58bc408e71f6405e834b6d7d083f50f7fa8119c
Holden Mills <mholdenmills@gmail.com>
2015-12-08 16:42:29 -0500

    Tie function working!

--- javascript.js
+++ javascript.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
     board: ['','','','','','','','',''],
     token: 'X'
   };
-  numMoves = 0;
+
   var box = function (cell) {
-        return cell === game.token;
-      };
+    return cell === game.token;
+  };
 
   var checkColumns = function checkColumns () {
     if (([game.board[0], game.board[3], game.board[6]].every(box)) ||
        ([game.board[1], game.board[4], game.board[7]].every(box)) ||
        ([game.board[2], game.board[5], game.board[8]].every(box)) ) {
       return true;
-      }
-      else {
-        return false;
-      }
-    };
+    }
+    else {
+      return false;
+    }
+  };
 
   var checkRows = function checkRows () {
     if (game.board.slice(0, 3).every(box) ||
@@ -51,34 +51,22 @@ $(document).ready(function() {
     return false;
     }
   };
-  // var playGame = function (element) {
+
   var checkTie = function checkTie() {
-    if (checkWinner === false && ([game.board.every(box) !== "" {
-      alert("It's a tie!  Play again!");
-    };
-
-
-{
-if(xTurn)
-{
-numMoves++;
-}
-else if(computerTurn == true)
-{
-status.innerHTML = 'X\'s turn';
-xTurn = true;
-computerTurn = false;
-numMoves++;
-square.value = 'O';
-}
-}
-else
-{
-alert('That square has already been played.');
-}
-
-  var gameCount = function gameCount() {
-    for (i=0; i<10 ;i++) {}
+    if (checkWinner() === false && game.board.every(function(mark) {
+      if (mark === 'X' || mark === 'O') {
+      return true;
+      }
+      else {
+        return false;
+      }
+    }) ) {
+    alert("It's a tie!  Play again!");
+      return true;
+    }
+    else {
+      return false;
+    }
   };
 
   var updateBoard = function updateBoard (index) {
@@ -101,16 +89,5 @@ alert('That square has already been played.');
   };
   $(".board").on("click",":button",function(event) {
     markCell(event);
-    console.log(game.board);
   });
 });
-
-
-
-// playGame(event.target);
-
-
-
-
-
-
